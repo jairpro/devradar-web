4ea72df3c255c7da7ef6150fa52c00c576e0b3ce
jairpro <jairedias@yahoo.com.br>
2020-01-26 01:14:31 -0300

    Ambientando a API_URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
+import { API_URL } from '../.env.json';
 
 const api = axios.create({
- //baseURL: 'http://localhost:3333'
- baseURL: 'http://backend.devradar.sistemanuvem.com'
+  baseURL: API_URL,
 });
 
 export default api;
